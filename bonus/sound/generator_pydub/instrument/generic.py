f9d9f876f9ef640d198f9a1491d0ff01195d987f
Henry LETELLIER <henry.letellier@epitech.eu>
2024-11-22 23:07:44 +0100

    [FIX] spelling mistake in the folder name intrument -> instrument

--- bonus/sound/generator_pydub/instrument/generic.py
+++ bonus/sound/generator_pydub/instrument/generic.py
@@ -75,10 +75,162 @@ except Exception as e:
     print("An error occurred:", e)
     process_ffmpeg_path()
 
-try:
-    from instrument_constants import NoteEquivalencePiano
-except ImportError as e:
-    raise Warning("Piano note equivalence not found.") from e
+DEFAULT_MUTE: int = -1
+DEFAULT_SAMPLE_RATE: int = 44100
+
+
+class NoteEquivalencePiano:
+    """
+        The class in charge of the piano notes and their frequencies.
+    """
+    SAMPLE_RATE = DEFAULT_SAMPLE_RATE
+    MAX_BIT_VALUE = 65535
+
+    MUTE = DEFAULT_MUTE
+
+    # 0 is used to represent the base note.
+    C0 = 16.35
+    CSH0 = 17.32
+    D0 = 18.35
+    Eb0 = 19.45
+    E0 = 20.6
+    F0 = 21.83
+    FSH0 = 23.12
+    G0 = 24.5
+    Ab0 = 25.96
+    A0 = 27.5
+    Bb0 = 29.14
+    B0 = 30.87
+
+    # 1 is used to represent the first octave.
+    C1 = 32.7
+    CSH1 = 34.65
+    D1 = 36.71
+    Eb1 = 38.89
+    E1 = 41.2
+    F1 = 43.65
+    FSH1 = 46.25
+    G1 = 49
+    Ab1 = 51.91
+    A1 = 55
+    Bb1 = 58.27
+    B1 = 61.74
+
+    # 2 is used to represent the second octave.
+    C2 = 65.41
+    CSH2 = 69.3
+    D2 = 73.42
+    Eb2 = 77.78
+    E2 = 82.41
+    F2 = 87.31
+    FSH2 = 92.5
+    G2 = 98
+    Ab2 = 103.83
+    A2 = 110
+    Bb2 = 116.54
+    B2 = 123.47
+
+    # 3 is used to represent the third octave.
+    C3 = 130.81
+    CSH3 = 138.59
+    D3 = 146.83
+    Eb3 = 155.56
+    E3 = 164.81
+    F3 = 174.61
+    FSH3 = 185
+    G3 = 196
+    Ab3 = 207.65
+    A3 = 220
+    Bb3 = 233.08
+    B3 = 246.94
+
+    # 4 is used to represent the fourth octave.
+    C4 = 261.63
+    CSH4 = 277.18
+    D4 = 293.66
+    Eb4 = 311.13
+    E4 = 329.63
+    F4 = 349.23
+    FSH4 = 369.99
+    G4 = 392
+    Ab4 = 415.3
+    A4 = 440
+    Bb4 = 466.16
+    B4 = 493.88
+
+    # 5 is used to represent the fifth octave.
+    C5 = 523.25
+    CSH5 = 554.37
+    D5 = 587.33
+    Eb5 = 622.25
+    E5 = 659.25
+    F5 = 698.46
+    FSH5 = 739.99
+    G5 = 783.99
+    Ab5 = 830.61
+    A5 = 880
+    Bb5 = 932.33
+    B5 = 987.77
+
+    # 6 is used to represent the sixth octave.
+    C6 = 1046.5
+    CSH6 = 1108.73
+    D6 = 1174.66
+    Eb6 = 1244.51
+    E6 = 1318.51
+    F6 = 1396.91
+    FSH6 = 1479.98
+    G6 = 1567.98
+    Ab6 = 1661.22
+    A6 = 1760
+    Bb6 = 1864.66
+    B6 = 1975.53
+
+    # 7 is used to represent the seventh octave.
+    C7 = 2093
+    CSH7 = 2217.46
+    D7 = 2349.32
+    Eb7 = 2489.02
+    E7 = 2637.02
+    F7 = 2793.83
+    FSH7 = 2959.96
+    G7 = 3135.96
+    Ab7 = 3322.44
+    A7 = 3520
+    Bb7 = 3729.31
+    B7 = 3951.07
+
+    # 8 is used to represent the eighth octave.
+    C8 = 4186.01
+    CSH8 = 4434.92
+    D8 = 4698.63
+    Eb8 = 4978.03
+    E8 = 5274.04
+    F8 = 5587.65
+    FSH8 = 5919.91
+    G8 = 6271.93
+    Ab8 = 6644.88
+    A8 = 7040
+    Bb8 = 7458.62
+    B8 = 7902.13
+
+    # Dictionary of notes
+    NOTE_EQUIVALENCE = {
+        "MUTE": MUTE,
+        "C": C4, "C#": CSH4, "D": D4, "D#": Eb4, "E": E4, "F": F4, "F#": FSH4, "G": G4, "G#": Ab4, "A": A4, "A#": Bb4, "B": B4,
+        "C0": C0, "C#0": CSH0, "D0": D0, "D#0": Eb0, "E0": E0, "F0": F0, "F#0": FSH0, "G0": G0, "G#0": Ab0, "A0": A0, "A#0": Bb0, "B0": B0,
+        "C1": C1, "C#1": CSH1, "D1": D1, "D#1": Eb1, "E1": E1, "F1": F1, "F#1": FSH1, "G1": G1, "G#1": Ab1, "A1": A1, "A#1": Bb1, "B1": B1,
+        "C2": C2, "C#2": CSH2, "D2": D2, "D#2": Eb2, "E2": E2, "F2": F2, "F#2": FSH2, "G2": G2, "G#2": Ab2, "A2": A2, "A#2": Bb2, "B2": B2,
+        "C3": C3, "C#3": CSH3, "D3": D3, "D#3": Eb3, "E3": E3, "F3": F3, "F#3": FSH3, "G3": G3, "G#3": Ab3, "A3": A3, "A#3": Bb3, "B3": B3,
+        "C4": C4, "C#4": CSH4, "D4": D4, "D#4": Eb4, "E4": E4, "F4": F4, "F#4": FSH4, "G4": G4, "G#4": Ab4, "A4": A4, "A#4": Bb4, "B4": B4,
+        "C5": C5, "C#5": CSH5, "D5": D5, "D#5": Eb5, "E5": E5, "F5": F5, "F#5": FSH5, "G5": G5, "G#5": Ab5, "A5": A5, "A#5": Bb5, "B5": B5,
+        "C6": C6, "C#6": CSH6, "D6": D6, "D#6": Eb6, "E6": E6, "F6": F6, "F#6": FSH6, "G6": G6, "G#6": Ab6, "A6": A6, "A#6": Bb6, "B6": B6,
+        "C7": C7, "C#7": CSH7, "D7": D7, "D#7": Eb7, "E7": E7, "F7": F7, "F#7": FSH7, "G7": G7, "G#7": Ab7, "A7": A7, "A#7": Bb7, "B7": B7,
+        "C8": C8, "C#8": CSH8, "D8": D8, "D#8": Eb8, "E8": E8, "F8": F8, "F#8": FSH8, "G8": G8, "G#8": Ab8, "A8": A8, "A#8": Bb8, "B8": B8
+    }
+
+
+DEBUG = True
 
 
 class Notes:
@@ -101,7 +253,7 @@ class Notes:
         FILE_DESCRIPTOR,
         SAVE_TO_FILE,
         FILE_NAME,
-        debug=False,
+        debug=DEBUG,
         logger="Notes"
     )
 
@@ -211,10 +363,10 @@ class Notes:
         if isinstance(frequency, str):
             frequency = Notes.NOTE_EQUIVALENCE.get(frequency.upper())
 
-        if frequency == Notes.MUTE:
+        if frequency == Notes.MUTE or isinstance(frequency, str) and frequency.upper() == "MUTE":
             return Notes.create_silent_note(duration, sample_rate)
 
-        if frequency is None or (frequency <= 0 or frequency > Notes.MAX_BIT_VALUE) and frequency != Notes.MUTE:
+        if frequency is None or (frequency <= 0 or frequency > Notes.MAX_BIT_VALUE) and frequency not in (Notes.MUTE, "MUTE"):
             msg = "Frequency must be a note or between 0"
             msg += f" and {Notes.MAX_BIT_VALUE}."
             raise ValueError(msg)
@@ -385,6 +537,7 @@ class Notes:
 
 if __name__ == "__main__":
     NODE = Notes
+    NODE.MUTE = NoteEquivalencePiano.MUTE
     NODE.NOTE_EQUIVALENCE = NoteEquivalencePiano.NOTE_EQUIVALENCE
     NODE.NOTE_DURATION = 0.2
     SILENCE_DURATION = 0.1
