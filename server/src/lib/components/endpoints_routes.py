0b7851f95faf1fb9432dceeef52276967cfc5beb
Henry LETELLIER <henry.letellier@epitech.eu>
2024-11-22 13:42:50 +0100

    [REMOVE] old server paths

--- server/src/lib/components/endpoints_routes.py
+++ server/src/lib/components/endpoints_routes.py
@@ -4,7 +4,7 @@
 from display_tty import Disp, TOML_CONF, FILE_DESCRIPTOR, SAVE_TO_FILE, FILE_NAME
 from .runtime_data import RuntimeData
 from .password_handling import PasswordHandling
-from .endpoints import Bonus, UserEndpoints, Services, Mandatory, Applets
+from .endpoints import Bonus, UserEndpoints, Applets
 
 
 class Endpoints:
@@ -44,19 +44,7 @@ class Endpoints:
             error=error,
             debug=debug
         )
-        self.services: Services = Services(
-            runtime_data=self.runtime_data_initialised,
-            success=self.success,
-            error=self.error,
-            debug=self.debug
-        )
         self.user_endpoints: UserEndpoints = UserEndpoints(
-            runtime_data=runtime_data,
-            success=success,
-            error=error,
-            debug=debug
-        )
-        self.mandatory: Mandatory = Mandatory(
             runtime_data=runtime_data,
             success=success,
             error=error,
@@ -72,7 +60,7 @@ class Endpoints:
     def inject_routes(self) -> None:
         """_summary_
         """
-        # Bonus routes
+        # Home route
         self.runtime_data_initialised.paths_initialised.add_path(
             "", self.bonus.get_welcome, [
                 "GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"
@@ -86,49 +74,10 @@ class Endpoints:
         self.runtime_data_initialised.paths_initialised.add_path(
             "/api/v1/", self.bonus.get_welcome, "GET"
         )
-        self.runtime_data_initialised.paths_initialised.add_path(
-            "/api/v1/bucket_names", self.bonus.get_s3_bucket_names, "GET"
-        )
-        self.runtime_data_initialised.paths_initialised.add_path(
-            "/api/v1/get_table", self.bonus.get_table, "GET"
-        )
+
+        # Manage the server stop
         self.runtime_data_initialised.paths_initialised.add_path(
             "/api/v1/stop", self.bonus.post_stop_server, "PUT"
-        )
-        self.runtime_data_initialised.paths_initialised.add_path(
-            "/api/v1/trigger_action/{id}", self.bonus.trigger_endpoint, "GET"
-        )
-
-        # Services routes
-        self.runtime_data_initialised.paths_initialised.add_path(
-            "/api/v1/services", self.services.get_services, "GET"
-        )
-        self.runtime_data_initialised.paths_initialised.add_path(
-            "/api/v1/service/name/{name}", self.services.get_service_name, "GET"
-        )
-        self.runtime_data_initialised.paths_initialised.add_path(
-            "/api/v1/service/{id}", self.services.get_service_id, "GET"
-        )
-        self.runtime_data_initialised.paths_initialised.add_path(
-            "/api/v1/services/{tags}", self.services.get_services_by_tag, "GET"
-        )
-        self.runtime_data_initialised.paths_initialised.add_path(
-            "/api/v1/recent_services", self.services.get_recent_services, "GET"
-        )
-        self.runtime_data_initialised.paths_initialised.add_path(
-            "/api/v1/service/{name}", self.services.create_service, "POST"
-        )
-        self.runtime_data_initialised.paths_initialised.add_path(
-            "/api/v1/service/{service_id}", self.services.update_service, "PUT"
-        )
-        self.runtime_data_initialised.paths_initialised.add_path(
-            "/api/v1/service/{service_id}", self.services.patch_service, "PATCH"
-        )
-        self.runtime_data_initialised.paths_initialised.add_path(
-            "/api/v1/service_id/name/{name}", self.services.get_service_id_by_name, "GET"
-        )
-        self.runtime_data_initialised.paths_initialised.add_path(
-            "/api/v1/service/{service_id}", self.services.delete_service, "DELETE"
         )
 
         # Authentication routes
@@ -190,7 +139,7 @@ class Endpoints:
         self.runtime_data_initialised.paths_initialised.add_path(
             "/api/v1/user_id", self.user_endpoints.get_user_id, "GET"
         )
-        
+
         # Applets routes
         self.runtime_data_initialised.paths_initialised.add_path(
             "/api/v1/applet", self.applets.create_applet, "POST"
@@ -227,9 +176,4 @@ class Endpoints:
         )
         self.runtime_data_initialised.paths_initialised.add_path(
             "/api/v1/reactions/{service_name}", self.applets.get_reactions_by_service_name, "GET"
-        )
-
-        # Mandatory routes
-        self.runtime_data_initialised.paths_initialised.add_path(
-            "/about.json", self.mandatory.get_about, "GET"
         )
